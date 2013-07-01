844002ffd9e66fd51962a56a55224d434343fa2b
Dima Sabanin <sdmitry@gmail.com>
2013-07-01 10:23:46 -0400

    Adding some docstrings

--- src/fastbeans/rpc.clj
+++ src/fastbeans/rpc.clj
@@ -4,6 +4,7 @@
    simplelog.use))
 
 (defn auto-resolve
+  "Resolves qualified symbol and tries to require namespace if missing."
   [f-str]
   (let [sym (symbol f-str)]
     (if-let [f-ns (-> sym namespace symbol)]
@@ -13,21 +14,26 @@
 (def cached-auto-resolve (memoize auto-resolve))
 
 (defn error?
+  "Is reply an error reply?"
   [res]
   (and (map? res)
        (contains? res :fastbeans-error)))
 
 (defn die
+  "Construct a death reply for client to know the error information."
   [error-id & args]
   (apply error (str "[" error-id "]") args)
   {:fastbeans-error error-id
    :error-information args})
 
 (defn prn-call
+  "Print call information."
   [f args]
   (str f " " (clojure.string/join " " (map str args))))
 
-(defn dispatch [[[signature [f-str & args :as whole]]]]
+(defn dispatch
+  "Dispatch incoming deserialized call and return the signature and result."
+  [[[signature [f-str & args :as whole]]]]
   (try
     (if-let [f (cached-auto-resolve f-str)]
       (benchmark f-str
